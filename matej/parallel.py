b5f946f701c82c02a65d44fc2b5171f1f2d1881a
Matej Vitek <matej.vitek.business@gmail.com>
2020-12-21 15:30:11 +0100

    Indentation

--- matej/parallel.py
+++ matej/parallel.py
@@ -5,30 +5,30 @@ from tqdm import tqdm
 
 @contextlib.contextmanager
 def tqdm_joblib(tqdm_object):
-    """Context manager to patch joblib to report into tqdm progress bar given as argument.
-    From: https://stackoverflow.com/a/58936697/5769814
-    
-    Example usage:
-    with tqdm_joblib(tqdm(files)) as data:
+	"""Context manager to patch joblib to report into tqdm progress bar given as argument.
+	From: https://stackoverflow.com/a/58936697/5769814
+
+	Example usage:
+	with tqdm_joblib(tqdm(files)) as data:
 		Parallel(n_jobs=-1)(
 			delayed(process_file)(f)
 			for f in data
 		)
-    """
-    class TqdmBatchCompletionCallback:
-        def __init__(self, time, index, parallel):
-            self.index = index
-            self.parallel = parallel
+	"""
+	class TqdmBatchCompletionCallback:
+		def __init__(self, time, index, parallel):
+			self.index = index
+			self.parallel = parallel
 
-        def __call__(self, index):
-            tqdm_object.update()
-            if self.parallel._original_iterator is not None:
-                self.parallel.dispatch_next()
+		def __call__(self, index):
+			tqdm_object.update()
+			if self.parallel._original_iterator is not None:
+				self.parallel.dispatch_next()
 
-    old_batch_callback = joblib.parallel.BatchCompletionCallBack
-    joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
-    try:
-        yield tqdm_object
-    finally:
-        joblib.parallel.BatchCompletionCallBack = old_batch_callback
-        tqdm_object.close()
+	old_batch_callback = joblib.parallel.BatchCompletionCallBack
+	joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
+	try:
+		yield tqdm_object
+	finally:
+		joblib.parallel.BatchCompletionCallBack = old_batch_callback
+		tqdm_object.close()
