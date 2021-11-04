74f126fda98b28fdbda5852577fc202df3dc2b66
Matej Vitek <matej.vitek.business@gmail.com>
2021-11-04 21:50:19 +0000

    Better tqdm_joblib that doesn't go over 100%

--- matej/parallel.py
+++ matej/parallel.py
@@ -1,35 +1,33 @@
 import contextlib
 import joblib
-from tqdm import tqdm
 
 
-# https://stackoverflow.com/a/58936697/5769814
+# https://stackoverflow.com/a/58936697/5769814 can get called on intermediate results,
+# so it ends up occasionally going over 100%. So we use this solution instead.
 @contextlib.contextmanager
 def tqdm_joblib(tqdm_object):
 	"""Context manager to patch joblib to report into tqdm progress bar given as argument.
-	From: https://stackoverflow.com/a/58936697/5769814
 
-	Example usage:
-	with tqdm_joblib(tqdm(files)) as data:
-		Parallel(n_jobs=-1)(
-			delayed(process_file)(f)
-			for f in data
-		)
+	From: https://stackoverflow.com/a/61689175/5769814
+
+	Sample usage::
+
+			with tqdm_joblib(tqdm(files)) as data:
+				Parallel(n_jobs=-1)(
+					delayed(process_file)(f)
+					for f in data
+				)
 	"""
-	class TqdmBatchCompletionCallback:
-		def __init__(self, time, index, parallel):
-			self.index = index
-			self.parallel = parallel
+	def tqdm_print_progress(self):
+		if self.n_completed_tasks > tqdm_object.n:
+			n_completed = self.n_completed_tasks - tqdm_object.n
+			tqdm_object.update(n=n_completed)
 
-		def __call__(self, index):
-			tqdm_object.update()
-			if self.parallel._original_iterator is not None:
-				self.parallel.dispatch_next()
+	original_print_progress = joblib.parallel.Parallel.print_progress
+	joblib.parallel.Parallel.print_progress = tqdm_print_progress
 
-	old_batch_callback = joblib.parallel.BatchCompletionCallBack
-	joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
 	try:
 		yield tqdm_object
 	finally:
-		joblib.parallel.BatchCompletionCallBack = old_batch_callback
+		joblib.parallel.Parallel.print_progress = original_print_progress
 		tqdm_object.close()
