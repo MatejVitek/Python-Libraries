e5389661d9efde1e6f4a83496e0659fdbe110c5b
Matej Vitek <matej.vitek.business@gmail.com>
2022-04-07 15:57:57 +0200

    Changed Config union operations to | from + to be consistent with python builtins

--- matej/__init__.py
+++ matej/__init__.py
@@ -111,30 +111,20 @@ class StoreDictPairs(argparse.Action):
 
 
 class Config(types.SimpleNamespace):
-	def __add__(self, other):
-		if isinstance(other, dict):
-			return Config(**{**self.__dict__, **other})
-		if isinstance(other, types.SimpleNamespace):
-			return Config(**{**self.__dict__, **other.__dict__})
-		raise TypeError(f"Addition not supported for types '{type(self)}' and '{type(other)}'.")
+	def __or__(self, other):
+		try:
+			return type(self)(**(self.__dict__ | other))
+		except TypeError as e:
+			raise TypeError(str(e).replace('dict', type(self).__name__)) from e
 
-	# This is implemented to support sum builtin
-	def __radd__(self, other):
-		if other == 0:
-			return self
-		if isinstance(other, dict):
-			return Config(**{**other, **self.__dict__})
-		if isinstance(other, types.SimpleNamespace):
-			return Config(**{**other.__dict__, **self.__dict__})
-		raise TypeError(f"Addition not supported for types '{type(self)}' and '{type(other)}'.")
+	def __ror__(self, other):
+		try:
+			return type(self)(**(other | self.__dict__))
+		except TypeError as e:
+			raise TypeError(str(e).replace('dict', type(self).__name__)) from e
 
-	def __iadd__(self, other):
-		if isinstance(other, dict):
-			self.__dict__.update(**other)
-		elif isinstance(other, types.SimpleNamespace):
-			self.__dict__.update(**other.__dict__)
-		else:
-			raise TypeError(f"Addition not supported for types '{type(self)}' and '{type(other)}'.")
+	def __ior__(self, other):
+		self.__dict__.update(**other)
 		return self
 
 	def __getitem__(self, key):
@@ -148,13 +138,13 @@ class Config(types.SimpleNamespace):
 
 	def __contains__(self, item):
 		return item in self.__dict__
-	
+
 	# Support **unpacking
 	def keys(self):
 		return self.__dict__.keys()
 
 	def copy(self):
-		return Config(**self.__dict__)
+		return type(self)(**self.__dict__)
 
 
 # Call as make_module_callable(__name__, function_to_call) at the end of the module definition
@@ -163,4 +153,3 @@ def make_module_callable(module_name, f):
 		def __call__(self, *args, **kw):
 			return f(*args, **kw)
 	sys.modules[module_name].__class__ = _CallableModule
-
