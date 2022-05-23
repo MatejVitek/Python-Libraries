68377c8c88113d21d13b45509eab3954e7cc95fb
Matej Vitek <matej.vitek.business@gmail.com>
2022-05-23 22:38:09 +0200

    Better way of specifying keys/values

--- matej/regedit.py
+++ matej/regedit.py
@@ -12,160 +12,142 @@ from typing import *
 import winreg as reg
 
 
-def _parse(path, default=True):
-	if default:
-		hive, *path = Path(path).parts
-		name = ''
-	else:
-		hive, *path, name = Path(path).parts
+def _parse(path):
+	hive, *path = Path(path).parts
 	shorthand = {'HKCR': 'HKEY_CLASSES_ROOT', 'HKLM': 'HKEY_LOCAL_MACHINE', 'HKCU': 'HKEY_CURRENT_USER', 'HKU': 'HKEY_USERS'}
 	if (hive := hive.upper()) in shorthand:
 		hive = shorthand[hive]
-	hive = getattr(reg, hive)
-	path = Path(*path)
-	return hive, path, name
+	return getattr(reg, hive), Path(*path)
 
 
-def has_value(path: Union[str, Path], default: bool = False) -> bool:
+def exists(key: Union[str, Path], value_name: Optional[str] = None) -> bool:
 	"""
-	Determine whether a key exists and contains the specified value.
+	Determines whether a key/value exists.
 
 	Args:
-		path: The full path to the value.
-		default: If `True`, look for the default value of the key specified by `path`.
+		key: The full path to the key.
+		value_name: The value name. If `None`, checks for the key only. If `''`, checks for the default value.
 
 	Returns:
 		`True` if value exists in specified key, `False` otherwise.
 	"""
 
-	return get_value(path, default) is not None
+	hive, path = _parse(key)
+	with suppress(OSError), reg.OpenKey(hive, str(path)) as key:
+		if value_name is not None:
+			reg.QueryValueEx(key, value_name)
+		return True
+	return False
 
 
-def get_value(path: Union[str, Path], default: bool = False, return_type: bool = False) -> Union[Optional[Union[str, int]], Tuple[Optional[Union[str, int]], Optional[int]]]:
+def get(key: Union[str, Path], value_name: str = '', return_type: bool = False) -> Union[Optional[Union[str, int]], Tuple[Optional[Union[str, int]], Optional[int]]]:
 	"""
-	Get the value from the specified registry path.
+	Gets the specified value from the specified key.
 
 	Args:
-		path: The full path to the value.
-		default: If `True`, get the default value of the key specified by `path`.
-		return_type: If `True`, will return the value type as well.
+		key: The full path to the key.
+		value_name: The value name. If `''`, get the default value.
+		return_type: If `True`, returns the value type as well.
 
 	Returns:
 		The value if it exists, `None` otherwise.
-		If `return_type` is `True`, will instead return a tuple of the value and its type,
+		If `return_type` is `True`, instead returns a tuple of the value and its type,
 		or `(None, None)` if the value does not exist.
 	"""
 
-	hive, path, name = _parse(path, default)
+	hive, path = _parse(key)
 	with suppress(OSError), reg.OpenKey(hive, str(path)) as key:
-		value, value_type = reg.QueryValueEx(key, name)
+		value, value_type = reg.QueryValueEx(key, value_name)
 		return (value, value_type) if return_type else value
 	return (None, None) if return_type else None
 
 
-def set_value(path: Union[str, Path], value: Union[str, int] = '', value_type: Union[str, int] = 'REG_SZ', default: bool = False, create_parent_keys: Optional[bool] = None):
+def set_value(key: Union[str, Path], value_name: str = '', value: Union[str, int] = '', value_type: Union[str, int] = 'REG_SZ', create_keys: Optional[bool] = None):
 	"""
-	Sets the value at the specified registry path.
+	Sets the specified value at the specified key.
 
 	Args:
-		path: The full path to the value.
-		value: The value to assign to the specified value path.
-		default: If `True`, set the default value of the key specified by `path`.
+		key: The full path to the key.
+		value_name: The value name. If `''`, sets the default value.
+		value: The value to assign to the specified value name.
 		value_type: The value type to use for the specified value. Defaults to `'REG_SZ'`.
-		create_parent_keys: Whether to create parent keys if they do not exist.
-			By default (or if `None` is passed), will only create the immediate parent.
-			If `False`, no parents will be created.
-			If `True`, all necessary parents will be created.
+		create_keys: Whether to create necessary keys if they do not exist.
+			By default (or if `None` is passed), only creates the key specified by `key` if it does not exist.
+			If `False`, no keys are created and this function fails if the key does not exist.
+			If `True`, the specified key and any non-existing parents are created.
 
 	Raises:
-		OSError: If the necessary parents did not exist and `create_parent_keys` was not `True`,
+		OSError: If the necessary keys did not exist (and were not created by this function as per `create_keys`),
 			or if the keys/value could not be created or set for some reason (such as permission errors).
 	"""
 
-	hive, path, name = _parse(path, default)
+	hive, path = _parse(key)
 	if isinstance(value_type, str):
 		value_type = getattr(reg, value_type)
-	if create_parent_keys:
+	if create_keys:
 		key = hive
 		for parent in path.parts:
 			with reg.CreateKey(key, parent) as key:
 				if parent == path.name:
-					reg.SetValueEx(key, name, 0, value_type, value)
-	elif create_parent_keys is None:
+					reg.SetValueEx(key, value_name, 0, value_type, value)
+	elif create_keys is None:
 		with reg.CreateKeyEx(hive, str(path)) as key:
-			reg.SetValueEx(key, name, 0, value_type, value)
+			reg.SetValueEx(key, value_name, 0, value_type, value)
 	else:
-		reg.SetValueEx(key, name, 0, value_type, value)
+		reg.SetValueEx(key, value_name, 0, value_type, value)
 
 
-def delete_value(path: Union[str, Path], default: bool = False):
+def delete_value(key: Union[str, Path], value_name: str = ''):
 	"""
-	Deletes the value at the specified registry path.
+	Deletes the specified value.
 
 	Args:
-		path: The full path to the value.
-		default: If `True`, delete the default value of the key specified by `path`.
+		key: The full path to the key.
+		value_name: The value name. If `''`, deletes the default value.
 
 	Raises:
 		OSError: If the specified value was not found or could not be deleted for some reason (such as permission errors).
 	"""
 
-	hive, path, name = _parse(path, default)
+	hive, path = _parse(key)
 	with reg.OpenKey(hive, str(path), access=reg.KEY_WRITE) as key:
-		reg.DeleteValue(key, name)
-		return True
+		reg.DeleteValue(key, value_name)
 
 
-def has_key(path: Union[str, Path]) -> bool:
+def delete_key(key: Union[str, Path], recursive: bool = False):
 	"""
-	Determines whether a key exists.
+	Deletes the specified key (and its values), which must not contain subkeys (unless `recursive=True` is passed).
 
 	Args:
-		path: The full path to the key.
-
-	Returns:
-		`True` if the key exists, `False` otherwise.
-	"""
-
-	hive, path, _ = _parse(path)
-	with suppress(OSError), reg.OpenKey(hive, str(path)):
-		return True
-	return False
-
-
-def delete_key(path: Union[str, Path], recursive: bool = False):
-	"""
-	Deletes the key (and its values) at the specified registry path.
-
-	Args:
-		path: The full path to the value.
-		recursive: If `True`, will delete subkeys as well.
-			If `False`, will fail if the specified key contains subkeys.
+		key: The full path to the key.
+		recursive: If `True`, recursively deletes subkeys as well.
+			If `False`, this function fails if the specified key contains subkeys.
 
 	Raises:
-		OSError: If the specified key was not found or could not be deleted for some reason (such as permission errors).
+		OSError: If the specified key was not found or contained subkeys (with `recursive=False`),
+			or could not be deleted for some other reason (such as permission errors).
 	"""
 
-	path = Path(path)
+	path = Path(key)
 	if recursive:
 		for subkey in subkeys(path):
 			delete_key(Path(path)/subkey, True)
-	hive, path, _ = _parse(path)
+	hive, path = _parse(path)
 	reg.DeleteKey(hive, str(path))
 
 
-def subkeys(path: Union[str, Path, int]) -> Iterator[str]:
+def subkeys(key: Union[str, Path, int]) -> Iterator[str]:
 	"""
 	Iterates over subkeys of a registry key.
 
 	Args:
-		path: The full path to the key.
+		key: The full path to the key.
 
 	Returns:
 		An iterator over all the subkey names of the specified key.
 	"""
 
-	hive, path, _ = _parse(path)
+	hive, path = _parse(key)
 	with suppress(OSError), reg.OpenKey(hive, str(path)) as key:
 		for i in it.count():
 			yield reg.EnumKey(key, i)
