6757835c5c48a81e458b1802aa4279967f9c5b89
Matej Vitek <matej.vitek.business@gmail.com>
2023-02-04 14:33:59 +0100

    Compose function and restoring the callable module function

---
+++ matej/callable.py
@@ -0,0 +1,21 @@
+import functools as ft
+import sys
+import types
+
+
+def compose(*functions):
+	"""
+	Compose arbitrary number of functions into one. I.e. compose(f, g, h)(x, y) == f(g(h(x, y))).
+
+	All functions, aside from the last one (h in the above example), must take exactly one argument.
+	"""
+
+	return ft.reduce(lambda f, g: lambda *args, **kwargs: f(g(*args, **kwargs)), functions)
+
+
+# Call as make_module_callable(__name__, function_to_call) at the end of the module definition
+def make_module_callable(module_name, f):
+	class _CallableModule(types.ModuleType):
+		def __call__(self, *args, **kw):
+			return f(*args, **kw)
+	sys.modules[module_name].__class__ = _CallableModule
