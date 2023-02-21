ed14815135c44f9f063cfa816ea2d027069a5145
Matej Vitek <matej.vitek.business@gmail.com>
2023-02-21 20:15:00 +0100

    Updates to setup files

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
+version = "0.1"
+
 setup(
-	name="matej-libs",
-	version="0.1",
+	name='matej-libs',
+	version=version,
 	author="Matej Vitek",
 	author_email="matej.vitek.business@gmail.com",
-	url="https://github.com/MatejVitek/Python-Libraries",
+	url='https://github.com/MatejVitek/Python-Libraries',
+	download_url=f'https://github.com/MatejVitek/Python-Libraries/archive/refs/tags/v{version}.tar.gz',
 	packages=find_packages(),
 	python_requires='>=3.8',
 	install_requires=['numpy'],
