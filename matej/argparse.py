52ba6eda6d6c79f21e529772144d4ac90345b486
Matej Vitek <matej.vitek.business@gmail.com>
2023-03-18 12:59:58 +0100

    Class now reformatted to contain Arg classes that are independent of the ArgParser class and can be used with other parsers as well

--- matej/argparse.py
+++ matej/argparse.py
@@ -28,11 +28,12 @@ class ArgParser(argparse.ArgumentParser):
 		self.register('action', 'store_dict', StoreDictPairsAction)
 
 	def add_arg(self, arg):
+		""" Add an :class:`Arg` instance to this parser. """
 		arg.add_to_ap(self)
 
 	def add_str_arg(self, *args, **kw):
 		"""
-		Add a string argument to the parser.
+		Auxiliary method for adding a :class:`StrArg` to the parser. See the documentation of :class:`StrArg` for details.
 
 		Examples
 		--------
@@ -43,7 +44,7 @@ class ArgParser(argparse.ArgumentParser):
 
 	def add_path_arg(self, *args, **kw):
 		"""
-		Add a path argument to the parser.
+		Auxiliary method for adding a :class:`PathArg` to the parser. See the documentation of :class:`PathArg` for details.
 
 		Examples
 		--------
@@ -54,18 +55,7 @@ class ArgParser(argparse.ArgumentParser):
 
 	def add_bool_arg(self, *args, **kw):
 		"""
-		Add a boolean argument to the parser.
-
-		This method adds:
-
-			- long flags that store `True` into the destination
-			- long flags with `--no-` prefix that store `False` into the destination
-			- short flags that:
-				- store `True` into the destination if `'True'` or `'Yes'` is passed as an argument;
-				- store `False` into the destination if `'False'` or `'No'` is passed as an argument;
-				- toggle the default value if no argument is passed.
-
-		If `dest` is not explicitly provided, it is inferred from the first long flag, or from the first short flag if no long flags are passed.
+		Auxiliary method for adding a :class:`BoolArg` to the parser. See the documentation of :class:`BoolArg` for details.
 
 		Examples
 		--------
@@ -76,16 +66,7 @@ class ArgParser(argparse.ArgumentParser):
 
 	def add_choice_arg(self, *args, **kw):
 		"""
-		Add a choice argument to the parser.
-
-		Parameters
-		----------
-		choices : Collection[Union[int, float, str]]
-			The possible choices.
-		choice_descriptions : Collection[str], optional
-			A list of descriptions for the choices.
-		type : type, optional
-			The type of the choices. If not provided, it is inferred from `choices` (`int` => `float` => `str`).
+		Auxiliary method for adding a :class:`ChoiceArg` to the parser. See the documentation of :class:`ChoiceArg` for details.
 
 		Examples
 		--------
@@ -96,20 +77,7 @@ class ArgParser(argparse.ArgumentParser):
 
 	def add_number_arg(self, *args, **kw):
 		"""
-		Add a number argument to the parser.
-
-		The argument can receive multiple values (in which case it will be stored as a list). This can be explicitly restricted by passing `nargs=1` to this method.
-
-		Parameters
-		----------
-		min : Union[int, float], optional
-			The minimum value the argument can have.
-		max : Union[int, float], optional
-			The maximum value the argument can have.
-		range : Tuple[Union[int, float], Union[int, float]], optional
-			A tuple of the minimum and maximum values the argument can have (alternative to `min` and `max`).
-		type : type, optional
-			The type of the argument. If not provided, it is inferred from the `min` and `max` values (`int` => `float`).
+		Auxiliary method for adding a :class:`NumberArg` to the parser. See the documentation of :class:`NumberArg` for details.
 
 		Examples
 		--------
@@ -121,38 +89,103 @@ class ArgParser(argparse.ArgumentParser):
 
 
 class Arg(ABC):
-	""" Base class for all arguments. """
+	"""
+	Base class for all arguments.
+
+	Instances of this class's subclasses can be added to an arbitrary :class:`argparse.ArgumentParser` instance
+	using the :meth:`add_to_ap` method,	although certain restrictions apply, as specified in the subclasses' docs.
+	"""
 	def __init__(self, *flags, **kw):
-		if not flags and 'dest' not in kw:
-			raise ValueError("You must provide a destination name for flagless arguments")
+		"""
+		Initialise the argument.
+
+		Parameters
+		----------
+		flags : Collection[str]
+			Optional flags for the argument. If no flags are provided, the `dest` keyword argument must be provided.
+		**kw
+			Keyword arguments to pass to the :meth:`argparse.ArgumentParser.add_argument` method.
+		"""
 		self.flags = flags
 		self.kw = kw
 
-	def add_to_ap(self, parser):
+	def add_to_ap(self, parser, **kw):
 		""" Add this argument to the given parser. """
-		return parser.add_argument(*self.flags, **self.kw)
+		kw = self.kw | kw
+		if not self.flags and 'dest' not in kw:
+			raise ValueError("You must provide a destination name for flagless arguments")
+		return parser.add_argument(*self.flags, **kw)
 
 
 class StrArg(Arg):
+	""" String argument. """
 	def __init__(self, *flags, **kw):
+		"""
+		Initialise the argument.
+
+		By default this argument is optional and accepts a single string as its value.
+
+		Parameters
+		----------
+		nargs : int or str, default='?'
+			Number of arguments to accept. See the documentation of :meth:`argparse.ArgumentParser.add_argument` for details.
+		type : type, default=str
+			Type of the argument.
+
+		For other parameters, see the documentation of :meth:`Arg.__init__`.
+		"""
 		kw.setdefault('nargs', '?')
 		kw.setdefault('type', str)
 		super().__init__(*flags, **kw)
 
 
 class PathArg(Arg):
+	""" Path argument. """
 	def __init__(self, *flags, **kw):
+		"""
+		Initialise the argument.
+
+		By default this argument is optional and accepts a single path as its value.
+
+		Parameters
+		----------
+		nargs : int or str, default='?'
+			Number of arguments to accept. See the documentation of :meth:`argparse.ArgumentParser.add_argument` for details.
+		type : type, default=Path
+			Type of the argument.
+
+		For other parameters, see the documentation of :meth:`Arg.__init__`.
+		"""
 		kw.setdefault('nargs', '?')
 		kw.setdefault('type', Path)
 		super().__init__(*flags, **kw)
 
 
 class BoolArg(Arg):
+	""" Boolean argument. """
 	def __init__(self, *flags, default=False, help="", negative_help=None, **kw):
+		"""
+		Initialise the argument.
+
+		Parameters
+		----------
+		default : bool, default=False
+			Default value for the argument.
+		help : str, default=""
+			Help string for the long positive form of the argument.
+		negative_help : str, default=None
+			Help string for the long negative form of the argument. If not provided, the negative form will simply prefix `help` with "Do not".
+		dest : str, optional
+			Destination in which to save the argument value (see the documentation of :meth:`argparse.ArgumentParser.add_argument` for details).
+			If not explicitly provided, it is inferred from the first long flag, or from the first short flag if no long flags are passed.
+
+		For other parameters, see the documentation of :meth:`Arg.__init__`.
+		"""
 		super().__init__(*flags, **kw)
 		self.short_flags = [flag for flag in flags if flag[0] == '-' and flag[1] != '-']
 		self.long_flags = [flag for flag in flags if flag[:2] == '--']
 		self.dest = kw.get('dest', (self.long_flags[0] if self.long_flags else flags[0]).strip('-').replace('-', '_'))
+		self.default = default
 
 		self.short_help = help + " (this toggles the default if no value is passed)" if help else help
 		self.yes_help = help
@@ -162,11 +195,24 @@ class BoolArg(Arg):
 		else:
 			self.no_help += " <default>"
 
-	def add_to_ap(self, parser):
+	def add_to_ap(self, parser, **kw):
+		"""
+		Add this argument to the parser.
+
+		This method adds:
+
+			- long flags that store `True` into the destination;
+			- long flags with `--no-` prefix that store `False` into the destination;
+			- short flags that:
+				- store `True` into the destination if `'True'` or `'Yes'` is passed as an argument,
+				- store `False` into the destination if `'False'` or `'No'` is passed as an argument,
+				- toggle the default value if no argument is passed.
+		"""
+		kw = self.kw | kw
 		group = parser.add_mutually_exclusive_group()
-		result = group.add_argument(*self.short_flags, dest=self.dest, nargs='?', default=self.default, const=not self.default, type=self._str_to_bool, help=self.short_help, **self.kw)
-		group.add_argument(*self.long_flags, dest=self.dest, action='store_true', help=self.yes_help, **self.kw)
-		group.add_argument(*map(self._no_f, self.long_flags), dest=self.dest, action='store_false', help=self.no_help, **self.kw)
+		result = group.add_argument(*self.short_flags, dest=self.dest, nargs='?', default=self.default, const=not self.default, type=self._str_to_bool, help=self.short_help, **kw)
+		group.add_argument(*self.long_flags, dest=self.dest, action='store_true', help=self.yes_help, **kw)
+		group.add_argument(*map(self._no_f, self.long_flags), dest=self.dest, action='store_false', help=self.no_help, **kw)
 		return result
 
 	@staticmethod
@@ -180,14 +226,36 @@ class BoolArg(Arg):
 
 #TODO: Make it possible to pass the choice descriptions as values too?
 class ChoiceArg(Arg):
+	""" Choice argument. """
 	def __init__(self, choices, *flags, choice_descriptions=(), type=None, help="", **kw):
+		"""
+		Initialise the argument.
+
+		Parameters
+		----------
+		choices : Collection[Union[int, float, str]]
+			The possible choices.
+		choice_descriptions : Collection[str], optional
+			A list of descriptions for the choices. If passed, should have the same length as `choices`.
+		type : type, optional
+			The type of the choices. If not provided, it is inferred from `choices` (`int` => `float` => `str`).
+		help : str, default=""
+			Help string for the argument. Extra information about the choices will be added to it automatically.
+			If `choice_descriptions` is not passed, this class should be used with a parser that uses the
+			:class:`HelpfulFormatter` as its help formatter, such as :class:`ArgParser`. Otherwise, the auto-generated
+			help will contain unformatted default value information.
+
+		For other parameters, see the documentation of :meth:`Arg.__init__`.
+		"""
 		super().__init__(*flags, **kw)
 		if type is None:
 			try:
 				type = int if all(int(x) == x for x in choices) else float
-			except ValueError:
+			except ValueError as e:
 				if all(isinstance(x, str) for x in choices):
 					type = str
+				else:
+					raise TypeError("Could not infer the type of the choices. Please pass the `type` argument.") from e
 		if choice_descriptions:
 			longest = max(len(str(choice)) for choice in choices)
 			help += "\n"
@@ -204,7 +272,32 @@ class ChoiceArg(Arg):
 
 
 class NumberArg(Arg):
+	""" Number argument. """
 	def __init__(self, *flags, min=None, max=None, range=None, type=None, help="", **kw):
+		"""
+		Initialise the argument.
+
+		The argument can receive multiple values (in which case it will be stored as a list). This can be explicitly restricted by passing `nargs=1` to this method.
+
+		Parameters
+		----------
+		action : str or :class:`argparse.Action`, default=ListOrSingleAction
+			The action to use for the argument. Should most likely be left as default but can be overridden if necessary.
+		nargs : int or str, default='+'
+			The number of arguments to parse. See the documentation of :meth:`argparse.ArgumentParser.add_argument` for more information.
+		min : Union[int, float], optional
+			The minimum value the argument can have.
+		max : Union[int, float], optional
+			The maximum value the argument can have.
+		range : Tuple[Union[int, float], Union[int, float]], optional
+			A tuple of the minimum and maximum values the argument can have (alternative to `min` and `max`).
+		type : type, optional
+			The type of the argument. If not provided, it is inferred from the `min` and `max` values (`int` => `float`).
+		help : str, default=""
+			Help string for the argument. Extra information about the minimum and maximum values will be added to it automatically.
+			This class should be used with a parser that uses the :class:`HelpfulFormatter` as its help formatter, such as :class:`ArgParser`.
+			Otherwise, the auto-generated help will contain unformatted range information.
+		"""
 		kw.setdefault('action', ListOrSingleAction)
 		kw.setdefault('nargs', '+')
 		super().__init__(*flags, **kw)
@@ -236,7 +329,6 @@ class NumberArg(Arg):
 
 class HelpfulFormatter(argparse.RawTextHelpFormatter):
 	""" Custom formatter that allows the use of certain action properties and respects \n in help messages. """
-
 	def _get_help_string(self, action):
 		help = super()._get_help_string(action)
 		format_dict = {
@@ -267,7 +359,6 @@ class HelpfulFormatter(argparse.RawTextHelpFormatter):
 
 class StoreDictPairsAction(argparse.Action):
 	""" Custom action to store key-value pairs in a dictionary. """
-
 	def __call__(self, parser, namespace, values, option_string=None):
 		d = getattr(namespace, self.dest)
 		if d is None:
@@ -290,7 +381,6 @@ class StoreDictPairsAction(argparse.Action):
 
 class ListOrSingleAction(argparse.Action):
 	""" Custom action that converts a list of length 1 into a single value for arguments that can receive multiple values. """
-
 	def __call__(self, parser, namespace, values, option_string=None):
 		values = ensure_iterable(values, str)
 		if len(values) == 1:
@@ -307,7 +397,8 @@ if __name__ == '__main__':
 	assert ap.parse_args(['asdf']).second_path_arg == Path('asdf')
 	assert ap.parse_args(['-p', 'qwer']).path_arg == Path('qwer')
 	_namespace = ap.parse_args(['asdf', '-p', 'qwer'])
-	assert _namespace.path_arg == Path('qwer') and _namespace.second_path_arg == Path('asdf')
+	assert _namespace.path_arg == Path('qwer')
+	assert _namespace.second_path_arg == Path('asdf')
 
 	# Bool args
 	ap.add_bool_arg('-t', '--true-arg', default=True, help="default true bool arg")
@@ -323,7 +414,8 @@ if __name__ == '__main__':
 	assert ap.parse_args(['--false-arg']).false_arg is True
 	assert ap.parse_args(['--no-false-arg']).false_arg is False
 	# _namespace = ap.parse_args(['-tf'])  # This one doesn't work
-	# assert _namespace.true_arg is False and _namespace.false_arg is True
+	# assert _namespace.true_arg is False
+	# assert _namespace.false_arg is True
 
 	# Choice args
 	ap.add_choice_arg((1, 2, 3), '-c', '--choice-arg', default=1, help="default 1 choice arg")
@@ -339,7 +431,8 @@ if __name__ == '__main__':
 	ap.add_number_arg('-m', '--number-arg', range=(0, 100.), nargs='+', help="number arg")
 	ap.add_number_arg('-n', '--number-arg2', min=0, default=40, help="default 40 number arg")
 	_namespace = ap.parse_args([])
-	assert _namespace.number_arg is None and _namespace.number_arg2 == 40
+	assert _namespace.number_arg is None
+	assert _namespace.number_arg2 == 40
 	assert ap.parse_args(['-m', '50']).number_arg == 50.
 	assert ap.parse_args(['-n', '610']).number_arg2 == 610
 	assert ap.parse_args(['-m', '50.5', '60', '70']).number_arg == [50.5, 60., 70.]
