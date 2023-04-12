c0e7986d65db09121db38d81bd10557fa7ee15f6
Matej Vitek <matej.vitek.business@gmail.com>
2023-04-12 18:00:26 +0200

    Uploaded to PyPI

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
-version = "0.1"
+version = "0.2"
 
 setup(
 	name='matej-libs',
 	version=version,
 	author="Matej Vitek",
 	author_email="matej.vitek.business@gmail.com",
+	description="Collection of Python utility libraries.",
 	url='https://github.com/MatejVitek/Python-Libraries',
-	download_url=f'https://github.com/MatejVitek/Python-Libraries/archive/refs/tags/v{version}.tar.gz',
 	packages=find_packages(),
 	python_requires='>=3.8',
 	install_requires=['numpy'],
