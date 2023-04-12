1579ac0c4ca7db627532a45257a1fd020bb56e01
Matej Vitek <matej.vitek.business@gmail.com>
2023-04-12 18:04:37 +0200

    Added description

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = "0.2"
+version = "0.1.1"
 
 setup(
 	name='matej-libs',
@@ -8,6 +8,7 @@ setup(
 	author="Matej Vitek",
 	author_email="matej.vitek.business@gmail.com",
 	description="Collection of Python utility libraries.",
+	long_description=open('README.md', encoding='utf-8').read(),
 	url='https://github.com/MatejVitek/Python-Libraries',
 	packages=find_packages(),
 	python_requires='>=3.8',
